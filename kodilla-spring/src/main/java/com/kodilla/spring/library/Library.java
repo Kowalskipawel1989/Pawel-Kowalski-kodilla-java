65ca6db5cc649e8fe801ad4de6561cc2048af05e
kowalskipawel1989 <kowalski.pawel.0304@gmail.com>
2020-10-14 07:12:56 +0200

    kodilla-course

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private final List<String> books = new ArrayList<>();
+    private final LibraryDbController libraryDbController;
+
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    public void saveToDb() {
+        libraryDbController.saveData();
+    }
+
+    public void loadFromDb() {
+        libraryDbController.loadData();
+    }
+}
