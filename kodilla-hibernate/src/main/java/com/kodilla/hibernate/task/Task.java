310d5041afdf6c4f9c1018df23f9d27de95c3a59
kowalskipawel1989 <kowalski.pawel.0304@gmail.com>
2020-11-24 19:13:26 +0100

    kodilla-course

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "TASKS")
-public class Task {
+public final class Task {
     private int id;
     private String description;
     private Date created;
@@ -20,6 +20,7 @@ public class Task {
         this.created = new Date();
         this.duration = duration;
     }
+
     @Id
     @GeneratedValue
     @NotNull
@@ -27,15 +28,18 @@ public class Task {
     public int getId() {
         return id;
     }
+
     @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
     }
+
     @NotNull
     @Column(name = "CREATED")
     public Date getCreated() {
         return created;
     }
+
     @Column(name = "DURATION")
     public int getDuration() {
         return duration;
