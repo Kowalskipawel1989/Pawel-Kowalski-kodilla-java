06f82474472d8eea6834b9ad6e607dea29837dc0
kowalskipawel1989 <kowalski.pawel.0304@gmail.com>
2021-04-13 12:28:06 +0200

    kodilla-course

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/bookclasifier/MedianAdapter.java
@@ -2,12 +2,25 @@ package com.kodilla.patterns2.adapter.company.bookclasifier;
 
 import com.kodilla.patterns2.adapter.company.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.company.bookclasifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.company.bookclasifier.libraryb.BookSignature;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
     public int publicationYearMedian(Set<Book> bookSet) {
-        return 0;
+        if (bookSet == null || bookSet.isEmpty()) {
+            throw new IllegalArgumentException("The set is empty");
+        }
+        return medianPublicationYear(bookSet.stream()
+                .collect(Collectors.toMap(MedianAdapter::toSignature, MedianAdapter::libraryA)));
+    }
+    private static com.kodilla.patterns2.adapter.company.bookclasifier.libraryb.Book libraryA(Book book) {
+        return new com.kodilla.patterns2.adapter.company.bookclasifier.libraryb.Book(
+                book.getAuthor(), book.getTitle(), book.getPublicationYear());
+    }
+    private static BookSignature toSignature(Book book) {
+        return new BookSignature(book.getSignature());
     }
 }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,10 +1,27 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
+import com.kodilla.patterns2.adapter.company.bookclasifier.MedianAdapter;
+import com.kodilla.patterns2.adapter.company.bookclasifier.librarya.Book;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedianTest() {
-
+        //Given
+        MedianAdapter medianAdapter = new MedianAdapter();
+        Set<Book> books = new HashSet<>();
+        books.add(new Book("Janek Wiśniewski","Black Rock",2006,"06"));
+        books.add(new Book("Roman Pawlak","Tango",2007,"05"));
+        books.add(new Book("Kamil Morawski","Little Princes",2003,"04"));
+        books.add(new Book("Jarosław Kowalski","Big Apple",2001,"03"));
+        books.add(new Book("Damian Stanisławski","Night",2002,"02"));
+        //When
+        int medianPublicationYear = medianAdapter.publicationYearMedian(books);
+        //Then
+        Assert.assertEquals(2003,medianPublicationYear);
     }
 }
